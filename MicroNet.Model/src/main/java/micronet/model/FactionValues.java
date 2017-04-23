424a4c7c687ac188ac4a64c62817b2f31a295808
Jonas <jonas_biedermann@hotmail.com>
2017-04-23 17:55:09 +0200

    Faction Values Sync with C#

--- MicroNet.Model/src/main/java/micronet/model/FactionValues.java
+++ MicroNet.Model/src/main/java/micronet/model/FactionValues.java
@@ -10,8 +10,8 @@ public class FactionValues {
 
 	public static class AttitudeThresholds {
 		public static final float[] rankPercentage = { 0.3f, 0.6f, 0.95f };
-		public static final float hostilePercentage = 0.2f;
-		public static final float hatefulPercentage = 0.8f;
+		public static final float hostilePercentage = -0.2f;
+		public static final float hatefulPercentage = -0.8f;
 	}
 
 	private float confederateReputation = 0;
@@ -40,8 +40,8 @@ public class FactionValues {
 	}
 
 	public int getAttitude() {
-		if (confederateReputation < -AttitudeThresholds.hatefulPercentage
-				&& rebelReputation < -AttitudeThresholds.hatefulPercentage)
+		if (confederateReputation < AttitudeThresholds.hatefulPercentage
+				&& rebelReputation < AttitudeThresholds.hatefulPercentage)
 			return Attitude.Outlaw;
 		if (confederateReputation > AttitudeThresholds.rankPercentage[0])
 			return Attitude.Confederate;
@@ -79,4 +79,67 @@ public class FactionValues {
 		if (rebelReputation > 1)
 			rebelReputation = 1;
 	}
+	
+    public int getRank()
+    {
+        if (getAttitude() == Attitude.Confederate)
+        {
+            if (confederateReputation > AttitudeThresholds.rankPercentage[2])
+                return 3;
+            if (confederateReputation > AttitudeThresholds.rankPercentage[1])
+                return 2;
+            if (confederateReputation > AttitudeThresholds.rankPercentage[0])
+                return 1;
+        }
+        if (getAttitude() == Attitude.Rebel)
+        {
+            if (rebelReputation > AttitudeThresholds.rankPercentage[2])
+                return 3;
+            if (rebelReputation > AttitudeThresholds.rankPercentage[1])
+                return 2;
+            if (rebelReputation > AttitudeThresholds.rankPercentage[0])
+                return 1;
+        }
+        return 0;
+    }
+    
+    public String getName()
+    {
+        if (getAttitude() == Attitude.Confederate)
+            return "Confederate";
+        if (getAttitude() == Attitude.Rebel)
+            return "Rebel";
+        if (getAttitude() == Attitude.Outlaw)
+            return "Outlaw";
+        return "Neutral";
+    }
+
+    public boolean isHostile(String faction)
+    {
+        switch (faction)
+        {
+            case "Outlaw":
+                if (getAttitude() == Attitude.Outlaw)
+                    return false;
+                return true;
+            case "Confederate":
+                if (getAttitude() == Attitude.Outlaw)
+                    return true;
+                if (getAttitude() == Attitude.Rebel)
+                    return true;
+                return false;
+            case "Rebel":
+                if (getAttitude() == Attitude.Outlaw)
+                    return true;
+                if (getAttitude() == Attitude.Confederate)
+                    return true;
+                return false;
+            case "Neutral":
+                if (getAttitude() == Attitude.Outlaw)
+                    return true;
+                return false;
+            default:
+                return false;
+        }
+    }
 }
